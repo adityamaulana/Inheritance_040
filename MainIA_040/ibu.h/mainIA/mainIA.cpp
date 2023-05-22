0b231484d5bc03b23e14b0a6565af7812afb7848
adityamaulana <aditmau26@gmail.com>
2023-05-22 18:01:19 +0700

    sec comm

--- MainIA_040/ibu.h/mainIA/mainIA.cpp
+++ MainIA_040/ibu.h/mainIA/mainIA.cpp
@@ -6,3 +6,12 @@ using namespace std;
 #include "Mom.h"
 #include "Kids.h"
 
+int main()
+{
+	Mom* varMom1 = new Mom ("Anya");
+	Mom* varMom2 = new Mom ("Anya");
+
+	Kids* varKids1 = new Kids ("Bono");
+	Kids* varKids2 = new Kids ("Mick Jagger");
+	Kids* varKids3 = new Kids ("Christ Martin");
+}
