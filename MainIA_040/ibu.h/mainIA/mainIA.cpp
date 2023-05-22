ffcfc3259c9663c9e2765dd117fbffc44e4c044f
adityamaulana <aditmau26@gmail.com>
2023-05-22 18:03:24 +0700

    third comm

--- MainIA_040/ibu.h/mainIA/mainIA.cpp
+++ MainIA_040/ibu.h/mainIA/mainIA.cpp
@@ -14,4 +14,14 @@ int main()
 	Kids* varKids1 = new Kids ("Bono");
 	Kids* varKids2 = new Kids ("Mick Jagger");
 	Kids* varKids3 = new Kids ("Christ Martin");
+
+	varMom1->addKid(varKids1);
+	varMom1->addKid(varKids2);
+	varMom2->addKid(varKids3);
+	varMom2->addKid(varKids1);
+
+	varMom1 -> printKid();
+	varMom2->printKid();
+
+	return 0;
 }
