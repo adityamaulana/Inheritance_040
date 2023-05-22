cde8c13d3ff8ea956b92dc9274a7705746cead6d
adityamaulana <aditmau26@gmail.com>
2023-05-22 17:23:02 +0700

    seventh comm

--- Inheritance_040/Inheritance_040.cpp
+++ Inheritance_040/Inheritance_040.cpp
@@ -12,11 +12,52 @@ public :
 	People(string pName) :
 		Name(pName) 
 	{
-		cout<<"People Made\n"<<endl;
+		cout<<"People Make\n"<<endl;
 	}
 
 	~People()
 	{
 		cout<<"People Remove\n"<<endl;
 	}
+
+	int Total (int a, int b)
+	{
+		return a + b;
+	}
 };
+
+
+
+class Student : public People
+{
+	public:
+		string School;
+
+		Student(string pName, string pSchool) :
+			People(pName),
+			School(pSchool)
+		{
+			cout<<"Student Make\n"<<endl;
+		}
+
+		~Student()
+		{
+			cout<<"Student Remove\n"<<endl;
+		}
+
+		string Intro()
+		{
+			return "Holla, Je sui Vincent " + Name + " From LBS" + School + "\n\n";
+		}
+};
+
+int main()
+{
+	Student Dummy1("Coki Pardede", "Gunadharma");
+
+	cout<<Dummy1.Intro();
+	cout << "Result = " << Dummy1.Total(10,90)<<endl;
+
+	return 0;
+}
+
